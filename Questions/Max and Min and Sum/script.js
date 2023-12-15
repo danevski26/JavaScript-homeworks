ac2c75f3bbca062a17e3a8861af2cc1244243a29
Kristijan Danevski <149868202+danevski26@users.noreply.github.com>
2023-12-15 13:43:19 +0100

    Add files via upload

---
+++ Questions/Max and Min and Sum/script.js
@@ -0,0 +1,22 @@
+let max ;
+let min ;
+let sum = max + min;
+
+function maxAndMin (arr){
+    for(let i=0;i<arr.legnth;i++){
+       max = arr[0]
+       if(arr[2]>max){
+        max = arr[i];
+       }
+       return max;
+    }
+   console.log (max)
+
+    
+}
+
+let array1=[56,32,102,44]
+
+maxAndMin (array1)
+
+// Ovde ne znam zosto "max" mi go printa kako undefined ako moze pomos.Blagodaram odnapred :)
